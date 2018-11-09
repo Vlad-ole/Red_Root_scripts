274672669ed8a6b65461086fb8c742ac4cc61236
Vladislav Oleynikov <oleynikov.vladislav.p@gmail.com>
2018-11-09 17:20:25 +0100

    works in debug!

--- Script_1/Source/main.cpp
+++ Script_1/Source/main.cpp
@@ -15,6 +15,7 @@
 #include "TStyle.h"
 #include "TTree.h"
 #include "TROOT.h"
+#include "TString.h"
 
 //Red
 #include "red-daq/EvRec0.hh"
@@ -29,36 +30,28 @@ using namespace std;
 int main(/*int argc, char *argv[]*/)
 {
     //start code
-    cout << "You are in program." << endl;
+    std::cout << "You are in program." << std::endl;
     TApplication* app = new TApplication("App",0,0);
 
 
-
     //main code
-    //TCanvas C1 = new TCanvas("canv name", "canv title", 500, 500);
-//    TGraph gr(100);
     TH1F *hist = new TH1F("test hist", "title", 1000, 0, 10);
     hist->FillRandom("gaus", 5000);
     hist->Draw("");
 
-    //gROOT->ProcessLine(".class EvRec0");
-
 
     ostringstream path_root_file;
     path_root_file << "/home/vlad/Soft/Red_Soft/red-daq-light/src/Level1/" << "run_" << 393 << ".root";
-    //cout << path_root_file.str().c_str() << endl; //error!?
-    //COUT(path_root_file);
+    std::cout << path_root_file.str().c_str() << std::endl;
 
-    //const char* filename = path_root_file.str().c_str();
+    TString filename = path_root_file.str().c_str();
 
-    //TFile *f = new TFile(path_root_file.str().c_str(), "read");
-    TFile *f = new TFile("/home/vlad/Soft/Red_Soft/red-daq-light/src/Level1/run_393.root", "read");
-
-//    if (!(f->IsOpen()))
-//    {
-//      cout << "could not open file: " << path_root_file.str().c_str() << endl;
-//      return 1;
-//    }
+    TFile *f = new TFile(filename, "read");
+    if (!(f->IsOpen()))
+    {
+      cout << "could not open file: " << filename << endl;
+      return 1;
+    }
 
 
 
@@ -71,5 +64,4 @@ int main(/*int argc, char *argv[]*/)
     //delete app;
 
     return 0;
-    //return a.exec();
 }
