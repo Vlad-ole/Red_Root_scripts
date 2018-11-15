5c5e3577caf957d7b39aafd13a3b61d4b4e2e94f
Vladislav Oleynikov <oleynikov.vladislav.p@gmail.com>
2018-11-15 10:43:25 +0100

    just stable

--- Script_1/Source/main.cpp
+++ Script_1/Source/main.cpp
@@ -50,9 +50,12 @@ int main(/*int argc, char *argv[]*/)
     //int run_number = 534; //ph2     backgroud
     int run_number = 537; //ph2     Am241
     //int run_number = 540; //ph2     Cf252
+    //int run_number = 542; //ph2     Am241
+    //int run_number = 544; //ph2     Am241
+    //int run_number = 554; //ph2     Am241 error
 
     //main code
-    ostringstream path_root_file;    
+    ostringstream path_root_file;
     path_root_file << "/media/vlad/Data/DS-data/reco/rm3reco/lns/camp_V/v3/" << "run_" << run_number << ".root";
     TString filename = path_root_file.str().c_str();
 
@@ -72,9 +75,12 @@ int main(/*int argc, char *argv[]*/)
     data->SetBranchAddress("recoevent",&evReco);
 
     TH2F *h2 = new TH2F("h2 name", "h2 title", 200, 0, 5, 200, 0, 5);//XY
-    //TH1F *h1 = new TH1F("h1 S1", "h1 title", 400, -100, 2000);//Am S1
-    //TH1F *h1 = new TH1F("h1 S2", "h1 title", 400, -100, 8000);//Am S2
-    TH1F *h1 = new TH1F("h1 S2/S1", "h1 title", 400, -1, 20);//Am S2/S1
+    TH1F *h1 = new TH1F("h1 S1", "h1 title", 400, -100, 2000);//Am S1 charge
+    //TH1F *h1 = new TH1F("h1 S2", "h1 title", 400, -100, 8000);//Am S2 charge
+    //TH1F *h1 = new TH1F("h1 S2/S1", "h1 title", 400, -1, 20);//Am S2_charge/S1_charge
+    //TH1F *h1 = new TH1F("h1 S2", "h1 title", 400, -100, 8000);//Am S2 tot_charge_top or tot_charge_bottom
+    //TH1F *h1 = new TH1F("h1 S2", "h1 title", 400, -100, 1000);//Am S1 tot_charge_top or tot_charge_bottom
+    //TH1F *h1 = new TH1F("h1 f90", "h1 title", 400, -0.2, 1);//f90
 
     for (int ev = 0; ev < data->GetEntries(); ev++)
     {
@@ -90,13 +96,17 @@ int main(/*int argc, char *argv[]*/)
             bool cls0_is_S1 = clusters.at(0)->f90 > 0.2;
             bool cls0_is_full = clusters.at(0)->rep == 1;
             bool S1_Am_peak = (clusters.at(0)->charge > 440) && (clusters.at(0)->charge < 630); // mean +- 1.5sigma using run 537
+            //bool S1_Am_peak = (clusters.at(0)->charge > 419) && (clusters.at(0)->charge < 587); // mean +- 1.5sigma using run 542
+            //bool S1_Am_peak = (clusters.at(0)->charge > 342) && (clusters.at(0)->charge < 482); // mean +- 1.5sigma using run 544
+            //bool S1_Am_peak = (clusters.at(0)->charge > 252) && (clusters.at(0)->charge < 366); // mean +- 1.5sigma using run 554
 
 
             for(int nc_i = 0; nc_i < nc; nc_i++)
             {
+
                 //cuts2
-                bool cls0 = nc_i == 0;
-                bool cls1 = nc_i == 1;
+                bool cls0 = nc_i == 0;//cluster 0
+                bool cls1 = nc_i == 1;//cluster 1
                 bool is_S1 =  nc == 2 && cls0 && cls0_is_full && cls0_is_S1;
                 bool is_S2 =  nc == 2 && cls1 && cls0_is_full && cls0_is_S1;
 
@@ -120,81 +130,89 @@ int main(/*int argc, char *argv[]*/)
                 bool corners = false;
 
                 //is_S2 && S1_Am_peak && edges
-//                REMEMBER_CUT();
+                REMEMBER_CUT(is_S2 && S1_Am_peak);
 
-//                if (total_cut) //cuts
+                if (total_cut) //cuts
+                {
+                    //cout << "   pos_x = " << clusters[nc_i]->pos_x << "; pos_y = " << clusters[nc_i]->pos_y << endl;
+                    h2->Fill(clusters.at(nc_i)->pos_x, clusters.at(nc_i)->pos_y);
+                    h1->Fill(clusters.at(nc_i)->pos_x);
+                }
+
+//                int channumber = 0;
+//                if (RDconfig::GetInstance()->GetChannelType(channumber) == RDconfig::kSiPMTop)
 //                {
-//                    //cout << "   pos_x = " << clusters[nc_i]->pos_x << "; pos_y = " << clusters[nc_i]->pos_y << endl;
-//                    h2->Fill(clusters.at(nc_i)->pos_x, clusters.at(nc_i)->pos_y);
-//                    h1->Fill(clusters.at(nc_i)->charge);
 //                }
-            }
+            }// end nc_i loop
 
-            REMEMBER_CUT(nc == 2 && cls0_is_full && cls0_is_S1 && S1_Am_peak);
-            if (total_cut) //cuts
-            {
-                h1->Fill(clusters.at(1)->charge / clusters.at(0)->charge);
-            }
+            //        REMEMBER_CUT(nc == 2 && cls0_is_full && cls0_is_S1 && S1_Am_peak);
+            //        if (total_cut) //cuts
+            //        {
+            //            h1->Fill(clusters.at(1)->charge / clusters.at(0)->charge);
+            //        }
 
+
+            //cout << endl;
         }
 
-        //cout << endl;
-
-
         if(ev%5000==0) cout << "Event " << ev << " processed" << endl;
-
     }
 
-    //Draw h1
+    //Draw options
+    bool is_draw_h1 = 0;
+    bool is_draw_h2 = 1;
 
+    if(is_draw_h1)
     {
         h1->SetTitle(total_cut_srt);
-        h1->GetXaxis()->SetTitle("clusters.at(1)->charge / clusters.at(0)->charge");
+        //h1->GetXaxis()->SetTitle("clusters.at(1)->charge / clusters.at(0)->charge");
+        h1->GetXaxis()->SetTitle("clusters.at(nc_i)->charge [PE]");
         h1->Draw();
     }
 
-    //Draw h2
-
+    if(is_draw_h2)
     {
-//        TCanvas *c1 = new TCanvas("c1","c1");
-//        //    c1->SetCanvasSize(850, 850);
-//        //    c1->SetWindowSize(900, 900);
-//        c1->Divide(2,1,0.01,0.01);
+        TCanvas *c1 = new TCanvas("c1","c1");
+        //    c1->SetCanvasSize(850, 850);
+        //    c1->SetWindowSize(900, 900);
+        c1->Divide(2,1,0.01,0.01);
 
 
-//        c1->cd(1);
-//        h2->SetTitle(total_cut_srt);
-//        h2->Draw("colz");
-//        gPad->Update();
-//        TPaveStats *st = (TPaveStats*)h2->GetListOfFunctions()->FindObject("stats");
-//        st->SetX1NDC(0.12); st->SetX2NDC(0.35);
-//        st->SetY1NDC(0.72); st->SetY2NDC(0.89);
-//        gPad->Modified(); gPad->Update();
+        c1->cd(1);
+        h2->SetTitle(total_cut_srt);
+        h2->GetXaxis()->SetTitle("x [cm]");
+        h2->GetYaxis()->SetTitle("y [cm]");
+        h2->Draw("colz");
+        gPad->Update();
+        TPaveStats *st = (TPaveStats*)h2->GetListOfFunctions()->FindObject("stats");
+        st->SetX1NDC(0.12); st->SetX2NDC(0.35);
+        st->SetY1NDC(0.72); st->SetY2NDC(0.89);
+        gPad->Modified(); gPad->Update();
 
-//        c1->cd(2);
-//        //TH2F *h2_copy = new TH2F(h2);
-//        TH2F *h2_copy = (TH2F*)h2->Clone("");//make clone to be free to delete statbox
-//        h2_copy->Draw("colz");
-//        h2_copy->SetStats(0); //delete statbox
-//        double yw = 0.8;//cm
-//        double xw = 1.2;//cm
-//        for (int iy = 0; iy < 6; iy++)
-//        {
-//            double x_centr;
-//            double y_centr;
-//            for (int ix = 0; ix < 4; ix++)
-//            {
-//                x_centr = ((0.5 + ix)*5/4);
-//                y_centr = ((0.5 + iy)*5/6);
+        c1->cd(2);
+        //TH2F *h2_copy = new TH2F(h2);
+        TH2F *h2_copy = (TH2F*)h2->Clone("");//make clone to be free to delete statbox
+        h2_copy->Draw("colz");
+        h2_copy->SetStats(0); //delete statbox
+        double yw = 0.8;//cm
+        double xw = 1.2;//cm
+        for (int iy = 0; iy < 6; iy++)
+        {
+            double x_centr;
+            double y_centr;
+            for (int ix = 0; ix < 4; ix++)
+            {
+                x_centr = ((0.5 + ix)*5/4);
+                y_centr = ((0.5 + iy)*5/6);
 
-//                TBox *box = new TBox(x_centr - xw/2, y_centr - yw/2, x_centr + xw/2, y_centr + yw/2);
-//                box->SetFillStyle(0);
-//                box->SetLineColor(2);
-//                box->SetLineWidth(2);
-//                box->Draw();
-//            }
-//        }
-//        gPad->Modified(); gPad->Update();
+                TBox *box = new TBox(x_centr - xw/2, y_centr - yw/2, x_centr + xw/2, y_centr + yw/2);
+                box->SetFillStyle(0);
+                box->SetLineColor(2);
+                box->SetLineWidth(2);
+                box->Draw();
+            }
+        }
+        gPad->Modified(); gPad->Update();
     }
 
 
